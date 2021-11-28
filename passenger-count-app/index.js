5141ae07a7fbf483bd2378e3625a4fa4983f8779
Khushi <58480229+khushi200701@users.noreply.github.com>
2021-11-29 00:43:37 +0530

    Update index.js

--- passenger-count-app/index.js
+++ passenger-count-app/index.js
@@ -1,6 +1,6 @@
-let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
     count += 1
@@ -13,5 +13,3 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
-
-console.log("Let's count people on the subway!")
